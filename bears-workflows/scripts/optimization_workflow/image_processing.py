2c450b45a094e6308e2a7be809555b1de46cd8d9
Jiaen <yeejiaen37@gmail.com>
2026-06-10 14:14:20 +0800

    update src parameter

--- bears-workflows/scripts/optimization_workflow/image_processing.py
+++ bears-workflows/scripts/optimization_workflow/image_processing.py
@@ -77,7 +77,7 @@ class ImageConfig:
 # Adjust src_corners if the camera is repositioned.
 # Adjust plate_width/plate_height to control warp output resolution.
 DEFAULT_CONFIG = ImageConfig(
-    src_corners=[(329, 346), (457, 347), (456, 431), (331, 430)],
+    src_corners=[(326, 286), (453, 287), (451, 369), (324, 369)],
     dst_corners=[(0, 0), (1800, 0), (1800, 1200), (0, 1200)],
     plate_width=1800,
     plate_height=1200,
